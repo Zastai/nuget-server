52ac89cac082683f22fef2edc1eb04d94ad12668
Tim Van Holder <tim.vanholder@gmail.com>
2021-12-10 18:20:19 +0100

    Add `NuGetService` record class
    This holds the information for a NuGet resource/service that is exposed
    via the Service Index.

---
+++ Zastai.NuGet.Server/NuGetService.cs
@@ -0,0 +1,9 @@
+﻿namespace Zastai.NuGet.Server;
+
+/// <summary>A NuGet service.</summary>
+/// <param name="Path">
+/// The path to the service; can be relative (for services implemented by this server) or absolute (for services hosted elsewhere).
+/// </param>
+/// <param name="Types">The service types (usually in <c>"name/version"</c> format) implemented by this service.</param>
+/// <param name="Description">A description of this service.</param>
+public sealed record NuGetService(string Path, IEnumerable<string> Types, string Description);
