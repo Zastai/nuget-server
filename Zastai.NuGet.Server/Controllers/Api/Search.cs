4c8d280cfd729ed9b6029ca8242d314473c4018b
Tim Van Holder <tim.vanholder@gmail.com>
2021-12-10 18:27:48 +0100

    Add `Search` service

---
+++ Zastai.NuGet.Server/Controllers/Api/Search.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zastai.NuGet.Server.Controllers.Api;
+
+/// <summary>A package search service.</summary>
+[Route(Search.BasePath)]
+public class Search : ApiController<Search> {
+
+  private const string BasePath = "search/query";
+
+  /// <summary>Creates a new search service controller.</summary>
+  /// <param name="logger">A logger for the controller.</param>
+  public Search(ILogger<Search> logger) : base(logger) {
+  }
+
+  #region NuGet Service Info
+
+  private const string Description = "Package search; supports filtering on package type.";
+
+  private static readonly string[] Types = {
+    "SearchQueryService",
+    "SearchQueryService/3.0.0-beta",
+    "SearchQueryService/3.0.0-rc",
+    "SearchQueryService/3.5.0", // Supports the packageType query parameter
+  };
+
+  /// <summary>NuGet service information for the search service.</summary>
+  public static readonly NuGetService Service = new(Search.BasePath, Search.Types, Search.Description);
+
+  #endregion
+
+}
