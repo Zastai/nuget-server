831395d3bfe5312c2d737bafeb49f07fe35af60c
Tim Van Holder <tim.vanholder@gmail.com>
2021-12-10 18:27:39 +0100

    Add `RepositorySignatures` service

---
+++ Zastai.NuGet.Server/Controllers/Api/RepositorySignatures.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zastai.NuGet.Server.Controllers.Api;
+
+/// <summary>A repository signatures service.</summary>
+[Route(RepositorySignatures.BasePath)]
+public class RepositorySignatures : ApiController<RepositorySignatures> {
+
+  private const string BasePath = "repository-signatures";
+
+  /// <summary>Creates a new repository signatures service controller.</summary>
+  /// <param name="logger">A logger for the controller.</param>
+  public RepositorySignatures(ILogger<RepositorySignatures> logger) : base(logger) {
+  }
+
+  #region NuGet Service Info
+
+  private const string Description = "The endpoint for discovering information about this package source's repository signatures.";
+
+  private static readonly string[] Types = {
+    "RepositorySignatures/4.7.0",
+    "RepositorySignatures/4.9.0",
+    "RepositorySignatures/5.0.0",
+  };
+
+  /// <summary>NuGet service information for the repository signatures service.</summary>
+  public static readonly NuGetService Service = new(RepositorySignatures.BasePath, RepositorySignatures.Types,
+                                                    RepositorySignatures.Description);
+
+  #endregion
+
+}
