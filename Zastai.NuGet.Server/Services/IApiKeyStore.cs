c369cea934c597147a1e6e3ece64b2bf2337f7aa
Tim Van Holder <tim.vanholder@gmail.com>
2022-01-05 22:13:20 +0100

    Add API keys and an API key store
    Currently the only implementation loads a list of keys from the app
    settings. Later, database-based stores can be added.

---
+++ Zastai.NuGet.Server/Services/IApiKeyStore.cs
@@ -0,0 +1,15 @@
+﻿namespace Zastai.NuGet.Server.Services;
+
+/// <summary>
+/// A store containing API keys.<br/>
+/// These will link to a user ID (with further information to be taken from an <see cref="IUserStore"/>) as well as access rights
+/// (one per operation secured via API key, such as "push package" and "delete/unlist package").
+/// </summary>
+public interface IApiKeyStore {
+
+  /// <summary>Finds a particular API key in the store.</summary>
+  /// <param name="id">The ID of the key to find.</param>
+  /// <returns>The requested API key, or <see langword="null"/> if none was found.</returns>
+  public Task<IApiKey?> FindKeyAsync(string id);
+
+}
---
+++ Zastai.NuGet.Server/Services/InMemoryApiKeyStore.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+
+using JetBrains.Annotations;
+
+namespace Zastai.NuGet.Server.Services;
+
+/// <summary>An API key store using an in-memory list (loaded from application settings).</summary>
+public class InMemoryApiKeyStore : IApiKeyStore {
+
+  /// <summary>Creates a new in-memory API key store.</summary>
+  /// <param name="config">The application settings to use.</param>
+  /// <param name="logger">The logger to use</param>
+  public InMemoryApiKeyStore(IConfiguration config, ILogger<InMemoryApiKeyStore> logger) {
+    var configuredKeys = config.GetSection("NuGet:ApiKeys")?.Get<Dictionary<string,ApiKey>?>();
+    if (configuredKeys is null) {
+      this._contents = ImmutableDictionary<string, ApiKey>.Empty;
+    }
+    else {
+      this._contents = configuredKeys;
+    }
+    logger.LogInformation("Configured API keys: {count}.", this._contents.Count);
+  }
+
+  #region IApiKeyStore
+
+  /// <inheritdoc />
+  public Task<IApiKey?> FindKeyAsync(string id)
+    => Task.FromResult<IApiKey?>(this._contents.TryGetValue(id, out var key) ? key : null);
+
+  #endregion
+
+  #region Internals
+
+  [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+  private sealed class ApiKey : IApiKey {
+
+    public bool CanDelete { get; set; }
+
+    public bool CanPublish { get; set; }
+
+    public DateTimeOffset Created { get; set; }
+
+    public DateTimeOffset Expiry { get; set; }
+
+    public string Id { get; set; } = "";
+
+    public string Name { get; set; } = "";
+
+    public string Owner { get; set; } = "";
+
+  }
+
+  private readonly IReadOnlyDictionary<string, ApiKey> _contents;
+
+  #endregion
+
+}
