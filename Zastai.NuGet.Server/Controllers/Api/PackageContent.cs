4f9844d33d210dcd6147d4831dca6d8354555d85
Tim Van Holder <tim.vanholder@gmail.com>
2022-08-15 23:41:02 +0200

    Implement retrieval of a package's nuspec file
    This reworks the file retrieval to use a single method on the package
    store (`GetFile` instead of `IsDownloadableFile` + `Open`), adding
    support for the package nuspec file.
    
    Some constants were moved to a project-global `Constants` class.

--- Zastai.NuGet.Server/Controllers/Api/PackageContent.cs
+++ Zastai.NuGet.Server/Controllers/Api/PackageContent.cs
@@ -11,8 +11,6 @@ public class PackageContent : ApiController<PackageContent> {
 
   private const string BasePath = "packages/content";
 
-  private const string PackageContentType = "application/octet-stream";
-
   private readonly IPackageStore _packageStore;
 
   /// <summary>Creates a new package download service controller.</summary>
@@ -38,33 +36,23 @@ public class PackageContent : ApiController<PackageContent> {
     return this.Json(new PackageVersions(versions));
   }
 
-  /// <summary>Retrieve a package.</summary>
+  /// <summary>Retrieve a file related to a package.</summary>
   /// <param name="id">The package ID.</param>
   /// <param name="version">The package version.</param>
-  /// <param name="file">The name of the package file to retrieve; must be a <c>.nupkg</c> or <c>.snupkg</c> file.</param>
-  /// <returns>The requested package if it is available; <c>404 - NOT FOUND</c> otherwise.</returns>
-  /// <response code="200">When the requested package is being returned.</response>
-  /// <response code="404">When the requested package is not available.</response>
+  /// <param name="file">
+  /// The name of the file to retrieve; must be either <c><paramref name="id"/>.nuspec</c>,
+  /// <c><paramref name="id"/>.<paramref name="version"/>.nupkg</c> or
+  /// <c><paramref name="id"/>.<paramref name="version"/>.snupkg</c>.
+  /// </param>
+  /// <returns>The requested file if it is available; <c>404 - NOT FOUND</c> otherwise.</returns>
+  /// <response code="200">When the requested file is being returned.</response>
+  /// <response code="404">When the requested file is not available.</response>
   [HttpGet("{id}/{version}/{file}")]
-  [ProducesResponseType(typeof(IEnumerable<byte>), StatusCodes.Status200OK, PackageContent.PackageContentType)]
+  [ProducesResponseType(typeof(IEnumerable<byte>), StatusCodes.Status200OK, Constants.PackageContentType)]
+  [ProducesResponseType(typeof(IEnumerable<byte>), StatusCodes.Status200OK, Constants.SpecContentType)]
+  [ProducesResponseType(typeof(IEnumerable<byte>), StatusCodes.Status200OK, Constants.SymbolsContentType)]
   [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
-  public IActionResult DownloadPackageFile(string id, string version, string file) {
-    // FIXME: Or should this validation be in PackageStore?
-    if (!this._packageStore.IsDownloadableFile(file)) {
-      this.Logger.LogWarning("Asked for a file from package {id} {version} that is not allowed for download ({file}).", id, version,
-                             file);
-      return this.NotFound();
-    }
-    // FIXME: Should this validate that id and version are lower case (and that version is normalized)?
-    // FIXME: Should this validate that file == {id}.{version}.[s]nupkg?
-    var package = this._packageStore.Open(id, version, file);
-    if (package is null) {
-      this.Logger.LogWarning("Asked for a file from package {id} {version} that is not available ({file}).", id, version, file);
-      return this.NotFound();
-    }
-    this.Logger.LogInformation("Returning a file from package {id} {version} ({file}).", id, version, file);
-    return this.File(package, PackageContent.PackageContentType, file);
-  }
+  public IActionResult DownloadPackageFile(string id, string version, string file) => this._packageStore.GetFile(id, version, file);
 
   #region NuGet Service Info
 
